96e7d6163b5574e0a232da4bb0134d8f59cc9d01
prachi <pmore@pods.com>
2025-04-30 18:15:30 -0400

    Created a dashboard page that shows the picture of the day. WIP

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,12 +3,41 @@ import {usePictureOfTheDay} from "../hooks/useNasaQueries";
 
 export const Dashboard: React.FC = () => {
     const {podResponse, isLoading: pictureOfTheDayLoading} = usePictureOfTheDay();
+    const styles = dashboardStyle();
     return (
-        <div className="dashboardScreen">
+        <div style={styles.dashboardScreen}>
             {pictureOfTheDayLoading && (<div>Loading...</div>)}
-            {podResponse && <div className="pictureOfTheDaySection">
-                <div>Title: {podResponse.title}</div>
-            </div>}
+            {podResponse &&
+                <>
+                    <div>Title: {podResponse.title}</div>
+                    <p>{podResponse.explanation}</p>
+                    <img style={styles.pictureOfTheDayImage} src={podResponse.hdurl ?? podResponse.url}
+                         alt={podResponse.title}/>
+                </>
+            }
         </div>
     );
 }
+
+const dashboardStyle = () => ({
+    dashboardScreen: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    } as React.CSSProperties,
+    pictureOfTheDaySection: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '20px',
+        border: '1px solid #ccc',
+        borderRadius: '5px',
+        margin: '10px',
+    },
+    pictureOfTheDaySectionTitle: {},
+    pictureOfTheDayImage: {
+        width: "70%",
+
+    }
+});
